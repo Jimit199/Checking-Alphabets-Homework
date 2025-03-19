123a2950719222c50342664e350de01182e79e87
Jimit199 <shahsd185@gmail.com>
2025-03-18 20:53:17 -0400

    Add files via upload

---
+++ Checking Aplhabets Homework.py
@@ -0,0 +1,5 @@
+char= input("Enter a Character:")
+if char=="a" or char=="b" or char=="c" or char=="d" or char=="e" or char=="f" or char=="g" or char=="h" or char=="i" or char=="j" or char=="k" or char=="l" or char=="m" or char=="p" or char=="q" or char=="r" or char=="s" or char=="t" or char=="u" or char=="v" or char=="w" or char=="x" or char=="y" or char=="z":
+    print(char,"is an alphabet")
+else:
+    print(char,"is not an alphabet")
